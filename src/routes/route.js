5a75ebb7339bb37f6309ac73fb89022f35d33f57
abhinav101901 <abhinav8299233838@gmail.com>
2022-12-12 16:05:22 +0530

    notes

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,140 +8,33 @@ const req = require('express/lib/request');
 const { route } = require('express/lib/application');
 
 
-router.get("/profile-details", function(req, res){
-    // Write the LOGIC here
-    res.send('dummy response')
-})
-
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
-
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
-    console.log(`The mentor of the day is ${mentorModule.mentor}`)
-
-    res.send('any dummy text from route handler 1')
-});
-
-
-router.get('/test-me', function(req, res){
-    console.log("I am here")
-    res.send("any dummy text from route handler 2")
-})
-
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
-// PATH Param example
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
-
-// PATH Param example
-router.get("/profile/:name", function(req, res){
-    console.log('Printing the request to find out wjere name is stored',req.params)
-    console.log('user name is',req.params.name)
-    //console.log(`User requesting for profile is ${name}`)
-    res.send("dummy details")
-})
-
-// Query Param example
-router.get("/shoes", function(req, res){
-    console.log("The filter options for shoes are -",req.query)
-    //req.query.size
-    //req.query.brand
-    res.send("dummy shoes response")
-})
-
-//==========================================================================================
-router.get('/film',function(req,res){
-    const movie = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-    res.send(movie) 
-})
-
-router.get('/film/:index',function(req,res){
-    const movie = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-    res.send(movie[req.params.index])
-})
-
-router.get('/film/:index',function(req,res){
-    const movie = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-    if(req.params.index > 3){
-        res.send("use a valid index")
-    }else
-    {res.send(movie[req.params.index])}
-    
-})
-
-router.get('/films',function(req,res){
-    const films = [ {
-       'id': 1,
-       'name': 'The Shining'
-      }, {
-       'id': 2,
-       'name': 'Incendies'
-      }, {
-       'id': 3,
-       'name': 'Rang de Basanti'
-      }, {
-       'id': 4,
-       'name': 'Finding Nemo'
-      }] 
-   let film =[]
-    films.forEach(a => {
-       film.push(a.name)
+router.get('/sumOfnumber',function(req,res){
+    const numbers = [1,2,3,4,5,7,8,9]
+    let n = numbers.length+1
+    const totalSum =(n*(n+1))/2
+    let sum =0
+    numbers.forEach(e => {
+        sum=sum+e
+        return sum
     })
-   res.send(film)   
+    const missingNumber = totalSum - sum
+    res.send(`Data ${missingNumber}`)
 })
 
 
-
-router.get('/films/:filmId',function(req,res){
-    const films = [ {
-        'id': 1,
-        'name': 'The Shining'
-       }, {
-        'id': 2,
-        'name': 'Incendies'
-       }, {
-        'id': 3,
-        'name': 'Rang de Basanti'
-       }, {
-        'id': 4,
-        'name': 'Finding Nemo'
-       }]
-    let i=0
-    const b=req.params.filmId
-    for(i=0;i<films.length;i++){
-        if(b==films[i].id){
-           return res.send(films[i].name)
-        }
-    }
-    if(i>=films.length){
-        res.send("No movie exists with this id")
-    }
-    res.send('I give you film Data')
+router.get('/missNumber',function(req,res){
+    let numbers = [33,34,35,37,38]
+    let n=numbers.length + 1
+    let first = numbers[0]
+    let last = numbers[numbers.length-1]
+    let totalSum =(n*(first + last))/2
+    let sum =0
+    numbers.forEach(e => {
+        sum=sum+e
+        return sum
+    })
+    const missingNumber = totalSum - sum
+    res.send(`Data ${missingNumber}`)
 })
 
 module.exports = router;
