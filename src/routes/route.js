b22f37ee0712e54a9ef1f5e26d2e20b93c66d767
abhinav101901 <abhinav8299233838@gmail.com>
2022-12-13 17:11:13 +0530

    player-data

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,34 +7,36 @@ const mentorModule = require('../abc/xyz/myModule');
 const req = require('express/lib/request');
 const { route } = require('express/lib/application');
 
-
-router.get('/sumOfnumber',function(req,res){
-    const numbers = [1,2,3,4,5,7,8,9]
-    let n = numbers.length+1
-    const totalSum =(n*(n+1))/2
-    let sum =0
-    numbers.forEach(e => {
-        sum=sum+e
-        return sum
-    })
-    const missingNumber = totalSum - sum
-    res.send(`Data ${missingNumber}`)
-})
-
-
-router.get('/missNumber',function(req,res){
-    let numbers = [33,34,35,37,38]
-    let n=numbers.length + 1
-    let first = numbers[0]
-    let last = numbers[numbers.length-1]
-    let totalSum =(n*(first + last))/2
-    let sum =0
-    numbers.forEach(e => {
-        sum=sum+e
-        return sum
-    })
-    const missingNumber = totalSum - sum
-    res.send(`Data ${missingNumber}`)
+const players = [{
+    "name" : "manish",
+    "dob":"1/1/1995",
+    "gender":"male",
+    "city":"Jalandhar",
+    "sports":["swimming"]
+},
+{
+    "name" : "gopal",
+    "dob":"1/09/1995",
+    "gender":"male",
+    "city":"delhi",
+    "sports":["soccer"]
+},{
+    "name" : "lokesh",
+    "dob":"1/1/1990",
+    "gender":"male",
+    "city":"mumbai",
+    "sports":["soccer"]
+}]
+router.post('/test-abhi',function(req,res){
+    let count=0
+    for(let i=0;i<players.length;i++){
+        if(players[i].name == req.body.name){
+            count++
+        }
+    }
+    if(count!=0){console.log("name is all ready present")}
+    if(count==0){console.log(Object.assign(players,req.body))}
+    res.send(req.body)
 })
 
 module.exports = router;
