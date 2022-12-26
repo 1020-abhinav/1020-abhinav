a26237aba579e74a050818447191056523852019
abhinav101901 <abhinav8299233838@gmail.com>
2022-12-26 22:53:01 +0530

    All

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const router = express.Router();
-//const userController= require("../controllers/userController")
+
 const Controller = require("../controllers/Contoller")
 
 const Middleware = require("../Middleware/middleware")
 
-// router.post("/users", userController.createUser  )
 
-// router.post("/login", userController.loginUser)
-
-// //The userId is sent by front end
-// router.get("/users/:userId", userController.getUserData)
-
-// router.put("/users/:userId", userController.updateUser)
 
 router.post("/user", Controller.createData )
 
 router.post("/logIn",Middleware.middle,Controller.Userlogin  )
 
-// router.post("/users", Controller.createUser  )
 
 router.get("/users/:userId",Middleware.middle,Controller.getUserData)
 
