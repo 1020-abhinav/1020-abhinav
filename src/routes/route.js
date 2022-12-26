b6b8dbbe4656267b9716985d9abd9fe23e3ff1e6
Abhinav Gupta <98967744+abhinav101901@users.noreply.github.com>
2022-12-26 12:59:43 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,10 +4,6 @@ const router = express.Router();
 const bookController= require("../controllers/bookController")
 
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
 router.post("/createAuthor", bookController.createAuthor  )
 
 router.post("/createPublisher", bookController.createPublisher  )
