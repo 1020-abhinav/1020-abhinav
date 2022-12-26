b3695449f1fa89c9a7f98d566b396c637817272c
Abhinav Gupta <98967744+abhinav101901@users.noreply.github.com>
2022-12-26 12:19:33 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,50 +1,7 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-// const Usercontroller= require("../controllers/usercontroller")
-// const Bookcontroller= require("../controllers/bookcontroller")
 const commonMW = require ("../middlewares/commonMiddlewares")
 const Controller= require("../controllers/controller")
-
-// router.get("/test-me", function (req, res) {
-//     res.send("My first ever api!")
-// })
-
-
-
-
-// router.post("/createBook", Bookcontroller.createBook  )
-
-
-
-
-// router.post("/createUser", Usercontroller.createUser  )
-// router.get("/getUsersData", Usercontroller.getUsersData)
-
-
-// const mid1= function ( req, res, next) {
-//     console.log("Hi I am a middleware named Mid1")
-//     // logic
-//     let loggedIn = false
-
-//     if (loggedIn== true) { 
-//         console.log( "OK LOGGED IS IS TRUE NOW")
-//         next ()
-//     }
-//     else {
-//         res.send ("Please login or register")
-//     }
-// }
-
-// // e.g. restricted and open-to-all API's can be handled like below now:
-// router.get('/homePage', mid1, Usercontroller.feeds)
-// router.get('/profileDetails', mid1, Usercontroller.profileDetails)
-// router.get('/friendList', mid1, Usercontroller.friendList)
-// router.get('/changePassword', mid1, Usercontroller.changePassword)
-
-// router.get('/termsAndConditions',  Usercontroller.termsAndConditions)
-// router.get('/register',  Usercontroller.register)
-
 
 router.get('/Request1', commonMW.middle,Controller.control)
 router.get('/Request2', commonMW.middle,Controller.contro2)
