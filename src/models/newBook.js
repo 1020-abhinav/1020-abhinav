00c673ccd96d3669d7bed9bf77d20e084936d0c4
Abhinav Gupta <98967744+abhinav101901@users.noreply.github.com>
2022-12-26 12:58:05 +0530

    Add files via upload

---
+++ src/models/newBook.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Author = require('./newAuthor') 
+const Publisher = require('./newPublisher')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const bookSchema = new mongoose.Schema( {
+    name: String,
+    author_id: {
+        type: ObjectId,
+        ref:"newAuthor"  
+    }, 
+    price: Number,
+    rating: Number,
+    publisher: {
+        type: ObjectId,
+        ref:"newPublisher"  
+    },
+    isHardCover:{
+        type:Boolean,
+        default:false
+    }
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('newBook', bookSchema)
